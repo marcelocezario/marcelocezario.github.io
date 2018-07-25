6d3864fce952ff5f050ff98342fa8f3098f91f0a
Marcelo Cezário <marcelocezario@gmail.com>
2018-07-25 00:55:43 -0300

    Incluindo informações e alterando o visual

--- js/gitHubApiClient.js
+++ js/gitHubApiClient.js
@@ -1,67 +1,56 @@
 var divRepositoriosDeck = $("#repoCardDeck");
 var str = "";
 
-function createRepoCard(repoName, repoDesc, repoUrl, repoForks, repoStars, repoWatchers, repoLanguage, repoUpdated_at) {
+function createRepoCard(repoName, repoDesc, repoUrl, repoForks, repoStars, repoWatchers, repoLanguage, repoUpdated_at, repoHomepage) {
     str +=
 
     "<div class='col-sm-4 d-flex p-2 bd-highlight'>"
+    +"  <div class='card text-center'>"
 
-    + "<div class='card text-center'>"
-    + "<div class='card-header'>"
-    +               repoName
-    + "                </div>"
-    + "        <div class='card-body'>"
-    + "            <p style='font-size: 12px'>" + repoDesc +"</p>"
-    
-    + "<small class='text-muted' style='center'>"
-    + "Linguagem: " + repoLanguage
-    + "       </small>"
+    +"      <div class='card-header'>"
+    +           repoName
+    +"      </div>"
 
-    
-    + "        </div>"
-    + "        <div class='card-footer text-muted'>"
+    +"      <div class='card-body'>"
+    +"          <p style='font-size: 12px'>"
+    +               repoDesc
+    +"          </p>"
+    +"          <small class='text-muted' style='center'>"
+    +"              Linguagem: "
+    +               repoLanguage
+    +"          </small>"
+    +"      </div>"
 
-    + "<small class='text-muted' style='center'>"
-    + "          <span class='fa fa-1x fa-star mb-3 sr-icons' style='margin:5px;'>"
-    + "              <span style='margin:3px;'>"
-    +                   repoStars 
-    + "              </span>"
-    + "          </span>"
-    + "         <span class='fa fa-1x fa-eye mb-3 sr-icons' style='margin:5px;'>"
-    + "             <span style='margin:3px;'>"
-    +                repoWatchers 
-    + "              </span>"
-    + "         </span>"
-    + "         <span class='fa fa-1x fa-code-fork mb-3 sr-icons' style='margin:5px;'>"
-    + "             <span style='margin:3px;'>"
-    +                repoForks
-    + "              </span>"
-    + "         </span>"
-    + "       </small>"
-    + "<div>"
-    + "<p style='font-size: 12px'>"
-    + "Updated "
-+"<local-time datetime='" + repoUpdated_at
-+"    month='short'"
-+"    day='numeric'"
-+"    year='numeric'"
-+"    hour='numeric'"
-+"    minute='numeric'>"
-+ repoUpdated_at
-+ "</local-time>"
+    +"      <div class='card-footer text-muted'>"
+    +"          <small class='text-muted' style='center'>"
+    +"              <span class='fa fa-1x fa-star mb-3 sr-icons' style='margin:5px;'>"
+    +"                  <span style='margin:3px;'>"
+    +                       repoStars 
+    +"                  </span>"
+    +"              </span>"
+    +"              <span class='fa fa-1x fa-eye mb-3 sr-icons' style='margin:5px;'>"
+    +"                  <span style='margin:3px;'>"
+    +                       repoWatchers 
+    +"                  </span>"
+    +"              </span>"
+    +"              <span class='fa fa-1x fa-code-fork mb-3 sr-icons' style='margin:5px;'>"
+    +"                  <span style='margin:3px;'>"
+    +                       repoForks
+    +"                  </span>"
+    +"              </span>"
+    +"          </small>"
+    +"          <div>"
+    +"              <p style='font-size: 12px'>"
+    +"                  Updated "
+    +                   repoUpdated_at
+    +"              </p>"
+    +"          </div>"
+    +"          <div>"
+    +"              <a href='" + repoUrl + "'>Acessar Repositório</a>"
+    +"          </div>"
+    +"      </div>"
 
-
-
-    +"</p>"
-    + "</div>"
-    +" <div>"
-    + "            <a href='" + repoUrl + "'>Acessar Repositório</a>"
-+"</div>"
-
-
-    
-    + "                </div>"
-    + "    </div>"
+    +"  </div>"
     +"</div>";
 }
 
@@ -83,15 +72,16 @@ $(document).ready(function () {
                 + ", Stars: " + repo.stargazers_count
                 + ", Watchers: " + repo.watchers_count
                 + ", Language: " + repo.language
-                + ", Update: " + repo.updated_at);
+                + ", Update: " + repo.updated_at
+                + ", Homepage: " + repo.homepage);
             if (i % 3 == 0) {
-                createRepoCard(repo.name, repo.description, repo.html_url, repo.forks_count, repo.stargazers_count, repo.watchers_count, repo.language, repo.updated_at);
+                createRepoCard(repo.name, repo.description, repo.html_url, repo.forks_count, repo.stargazers_count, repo.watchers_count, repo.language, repo.updated_at, repo.homepage);
                 str += "</div>";
                 str += "</p>";
                 str += "<div class='row'>";
             }
             else {
-                createRepoCard(repo.name, repo.description, repo.html_url, repo.forks_count, repo.stargazers_count, repo.watchers_count, repo.language, repo.updated_at);
+                createRepoCard(repo.name, repo.description, repo.html_url, repo.forks_count, repo.stargazers_count, repo.watchers_count, repo.language, repo.updated_at, repo.homepage);
             }
             i++;
         });
